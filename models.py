1c015ae3d4318f0283dad7051021fcc80fe4df66
charlieRedfearn <146747971+charlieRedfearn@users.noreply.github.com>
2025-02-27 14:24:54 +0000

    Partial database - no relationships

---
+++ models.py
@@ -0,0 +1,71 @@
+from app import db
+
+class user(db.model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.Integer, default = 1) #1 = user, 2 = expert 3 = manager
+
+    #relationships
+    notifications = db.relationship('notification', back_populates='author', cascade="all, delete-orphan")
+    items = db.relationship('item', )
+
+
+class item(db.model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(200))
+    starting_price= db.Column(db.Integer)
+    duration= db.Column(db.Integer)
+    auction_end = db.Column(db.DateTime)
+    authenticated = db.Column(db.Boolean)
+
+class bid(db.model):
+    __tablename__ = 'bids'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer)
+    bid_time = db.Column (db.DateTime)
+
+class payment(db.model):
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True)
+    amount_paid = db.Column(db.Integer)
+    payment_status = db.Column(db.Boolean)
+    payment_time = db.Column(db.DateTime)
+
+class notification(db.model):
+    __tablename__ = 'notifications'
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime)
+    is_read = db.Column(db.Boolean)
+
+class request(db.model):
+    __tablename__ = 'requests'
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Boolean)
+    fee_percentage = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+
+class expertAvailablity(db.model):
+    __tablename__ = 'experts'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+
+class setting(db.model):
+    __tablename__ = 'settings'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    value = db.Column(db.Integer)
+
+class report(db.model):
+    __tablename__ = 'reports'
+    id = db.Column(db.Integer, primary_key=True)
+    week_start = db.Column(db.DateTime)
+    week_end = db.Column(db.DateTime)
+    total_sales = db.Column(db.Integer)
+    total_fees = db.Column(db.Integer)
